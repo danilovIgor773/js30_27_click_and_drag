39b873e2fae75e9f9395c4ef429b76eaf66da63f
Danilov Igor <danilovigor773@gmail.com>
2018-10-16 09:12:31 +0500

    Lesson 27 of js30 course. Click and drag. Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 import './index.scss';
 
-console.log("Hello World");
+const slider = document.querySelector('.items');
+//console.log(slider);
+let startX;
+let isDown = false;
+let scrollLeft;
+
+slider.addEventListener('mouseup', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
+
+slider.addEventListener('mousedown', (e) => {
+  isDown = true;
+  slider.classList.add('active');
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+  //console.log({startX, scrollLeft});
+});
+
+slider.addEventListener('mousemove', (e) => {
+  if(!isDown){
+    return;
+  }
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = x - startX;
+  console.log(walk);
+  slider.scrollLeft = scrollLeft - walk;
+});
+
+slider.addEventListener('mouseleave', () => {
+  isDown = false;
+  slider.classList.remove('active');
+});
